fffaa228bdec1900d13b90235d3c3d4c743a3183
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-03-30 15:42:03 +0200

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Patrick Battisti Forsthofer on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
