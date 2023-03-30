c86db21b914df20f7800b66c9b86e9d797b96630
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-03-30 16:06:21 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Patrick Battisti Forsthofer on 30/03/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
