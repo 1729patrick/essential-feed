110afb9c96c11933c81509e6b7114f6a2104c2d7
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-04-15 15:24:20 +0200

    Move ViewModels to separate files

---
+++ EssentialFeediOS/FeedUI/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Patrick Battisti Forsthofer on 15/04/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
