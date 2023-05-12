cb0feb750c7d617b3b8001923bbafb7ae7688114
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-05-12 22:10:08 +0200

    Sync code with off screen changes

--- EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-// Created by Patrick Battisti Forsthofer on 12/05/2023
-// Copyright © 2023 Patrick Battisti Forsthofer. All rights reserved.
-//
-
-public struct FeedLoadingViewModel {
-	public let isLoading: Bool
-}
