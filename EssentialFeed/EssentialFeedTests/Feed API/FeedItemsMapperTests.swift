0cb6009903f371f6aeee2725934e229bfb13a996
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-05-19 11:41:05 +0200

    Test FeedItemsMapper in isolation

--- EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -1,6 +1,5 @@
 //
-// Created by Patrick Battisti Forsthofer on 12/05/2023
-// Copyright © 2023 Patrick Battisti Forsthofer. All rights reserved.
+//  Copyright © 2018 Essential Developer. All rights reserved.
 //
 
 import XCTest
