24e6539b7b4b4932deac307a446cecd5c552d84b
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-05-26 21:53:15 +0200

    Remove unused FeedImagePresenter logic

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -2,12 +2,10 @@
 // Created by Patrick Battisti Forsthofer on 12/05/2023
 // Copyright © 2023 Patrick Battisti Forsthofer. All rights reserved.
 //
-public struct FeedImageViewModel<Image> {
+
+public struct FeedImageViewModel {
 	public let description: String?
 	public let location: String?
-	public let image: Image?
-	public let isLoading: Bool
-	public let shouldRetry: Bool
 	
 	public var hasLocation: Bool {
 		return location != nil
