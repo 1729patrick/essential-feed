5dd3ee6b3959ff212295c901df0ae7ba0466c35b
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-06-04 21:51:03 +0200

    Load more items on will display LoadMoreCell

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -104,6 +104,14 @@ extension ListViewController {
 		let index = IndexPath(row: row, section: feedImagesSection)
 		ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
 	}
+    
+    func simulateLoadMoreFeedAction() {
+        guard let view = cell(row: 0, section: feedLoadMoreSection) else { return }
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: 0, section: feedLoadMoreSection)
+        delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
+    }
 	
 	func renderedFeedImageData(at index: Int) -> Data? {
 		return simulateFeedImageViewVisible(at: index)?.renderedImage
@@ -118,4 +126,5 @@ extension ListViewController {
 	}
 	
 	private var feedImagesSection: Int { 0 }
+    private var feedLoadMoreSection: Int { 1 }
 }
